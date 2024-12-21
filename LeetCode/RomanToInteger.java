9768eab9a3698c6191f4ff21fa5bbf663d3dc6c9
Jesus Betancourt <jbeta02@gmail.com>
2024-12-20 17:55:18 -0800

    roman to int

---
+++ LeetCode/RomanToInteger.java
@@ -0,0 +1,46 @@
+// LeetCode 13. Roman to Integer
+// Time: O(n)
+// Space: O(1)
+
+import java.util.HashMap;
+
+class Solution {
+    public int romanToInt(String s) {
+        HashMap<Character, Integer> romanMapping = new HashMap<Character, Integer>();
+
+        romanMapping.put('I', 1);
+        romanMapping.put('V', 5);
+        romanMapping.put('X', 10);
+        romanMapping.put('L', 50);
+        romanMapping.put('C', 100);
+        romanMapping.put('D', 500);
+        romanMapping.put('M', 1000);
+
+        char curr;
+        char next;
+        int total = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            curr = s.charAt(i);
+
+            if (i + 1 > s.length() - 1) {
+                total += romanMapping.get(curr);
+                break;
+            }
+
+            next = s.charAt(i + 1);
+
+            if (romanMapping.get(curr) < romanMapping.get(next)){
+                total -= romanMapping.get(curr);
+            }
+
+            else {
+                total += romanMapping.get(curr);
+            }
+            
+        }
+
+        return total;
+    }
+}
