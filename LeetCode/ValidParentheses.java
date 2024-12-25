77dab25ef48a7c7c50f042897ec0280a5182f6c3
Jesus <jbeta02@gmail.com>
2024-12-24 17:49:39 -0800

    valid parentheses

---
+++ LeetCode/ValidParentheses.java
@@ -0,0 +1,59 @@
+// 20. Valid Parentheses
+// time: O(n)
+// space: O(1)
+
+import java.util.Stack;
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
+
+        if (s.length() % 2 != 0) {
+            return false;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            char curr = s.charAt(i);
+            if (curr == '(' || curr == '[' || curr == '{') {
+                stack.push(curr);
+            }
+            
+            else if (curr == ')' || curr == ']' || curr == '}') {
+                
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
+                else if (curr == ')') {
+                    if (stack.peek() == '('){
+                        stack.pop();
+                    }
+                    else {
+                        return false;
+                    }
+                }
+                else if (curr == ']') {
+                    if (stack.peek() == '['){
+                        stack.pop();
+                    }
+                    else {
+                        return false;
+                    }
+                }
+                else if (curr == '}') {
+                    if (stack.peek() == '{'){
+                        stack.pop();
+                    }
+                    else {
+                        return false;
+                    }
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
